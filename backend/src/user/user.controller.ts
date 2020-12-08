042ab1ae8e112b232994f11f7201860f924053ee
JCases <37308043+JCases@users.noreply.github.com>
2020-12-08 02:46:55 +0100

    Actualizado Backend

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,8 +1,50 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { IRequest } from 'src/shared/interfaces/request';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Body,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { IUser } from './../../../global';
+import { encode } from '../auth/auth';
+import { IsLoggedGuard } from '../shared/guards/isLogged.guard';
+import { IRequest } from 'src/shared/interfaces/request';
 
 @Controller('user')
 export class UserController {
   constructor(protected userService: UserService) {}
+
+  @Post('login')
+  public async login(@Body() body: IUser) {
+    if (!body.email)
+      throw new HttpException('Email es requerido', HttpStatus.NOT_ACCEPTABLE);
+    return {
+      token: encode(await this.userService.login(body)),
+    };
+  }
+
+  @Get('token')
+  @UseGuards(IsLoggedGuard)
+  public token() {
+    return true;
+  }
+
+  @Post('register')
+  public async register(@Body() body: IUser) {
+    return {
+      token: encode(await this.userService.register(body)),
+    };
+  }
+
+  @Get('rehydrate')
+  @UseGuards(IsLoggedGuard)
+  public async rehydrate(@Req() req: IRequest) {
+    return {
+      token: encode(await this.userService.rehydrate(req.user)),
+    };
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { IUser } from '../../../global';
 import { User } from '../user/user.model';
 
 @Injectable()
@@ -16,5 +17,79 @@ export class UserService {
         attributes: { exclude: ['deletedAt'] },
       });
     }
+  }
+  async setPhoneInfo(user: IUser, id: string) {
+    const {
+      appBuild,
+      appVersion,
+      manufacturer,
+      operatingSystem,
+      platform,
+      uuid,
+      osVersion,
+      model,
+    } = user;
+    try {
+      return await User.update(
+        {
+          appBuild,
+          appVersion,
+          manufacturer,
+          operatingSystem,
+          platform,
+          uuid,
+          osVersion,
+          model,
+        },
+        { where: { id } },
+      );
+    } catch (e) {
+      throw new HttpException(
+        'No se ha podido guardar la información, intentalo más adelante',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+  }
+  async login(user: IUser) {
+    const userDB = await User.findOne({
+      where: { email: user.email! },
+      attributes: ['email', 'id'],
+    });
+    if (userDB) {
+      return userDB;
+    }
+    throw new HttpException('Usuario incorrecto', HttpStatus.UNAUTHORIZED);
+  }
+
+  async rehydrate(userToken: IUser) {
+    if (userToken) {
+      const user = await User.findByPk(userToken.id, {
+        attributes: { exclude: ['password', 'deletedAt'] },
+      });
+
+      if (user) return user;
+      throw new HttpException('No estás logueado', HttpStatus.UNAUTHORIZED);
+    }
+    throw new HttpException('No estás logueado', HttpStatus.UNAUTHORIZED);
+  }
+
+  async register(user: IUser) {
+    const userDB = await User.findOne({
+      where: { email: user.email! },
+      attributes: ['id'],
+    });
+    if (!userDB) {
+      delete user.id;
+      delete user.root;
+      try {
+        return User.create(user);
+      } catch (e) {
+        throw new HttpException(
+          'No se ha podido crear el usuario',
+          HttpStatus.NOT_ACCEPTABLE,
+        );
+      }
+    }
+    throw new HttpException('El email ya esta en uso', HttpStatus.FORBIDDEN);
   }
 }
